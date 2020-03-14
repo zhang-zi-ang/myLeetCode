e01bc5908ae90008571481058fdf07cceaf848e9
zhang-zi-ang <917960894@qq.com>
2020-03-14 22:11:35 +0800

    Initial Commit

---
+++ myLeetCode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  myLeetCode
+//
+//  Created by 张子昂 on 2020/3/14.
+//  Copyright © 2020 张子昂. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
